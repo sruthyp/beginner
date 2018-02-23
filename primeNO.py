8f5cbaa73bbb70dd8e770243f829ee1db565ffa2
sruthyp <36729994+sruthyp@users.noreply.github.com>
2018-02-23 16:01:25 +0530

    Create primeNO.py

---
+++ primeNO.py
@@ -0,0 +1,12 @@
+num=int(raw_input("Enter a number "))
+if num > 1:
+ 	for i in range(2,num):
+       		if (num % i) == 0:
+           		print num,"is not a prime number"
+           		print i,"is a factor of ",num
+           		break
+   		else:
+       			print num,"is a prime number"
+
+else:
+   	print num,"is not a prime number"
