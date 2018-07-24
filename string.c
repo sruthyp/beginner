3cffaa44fc5b97da99c4ce3d724d2821ab145660
sruthyp <36729994+sruthyp@users.noreply.github.com>
2018-07-24 16:10:06 +0530

    Create string.c

---
+++ string.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include<string.h>
+int main() 
+{
+    char a[10];
+    int flag=0,i;
+    scanf("%s",a);
+    int n=strlen(a);
+    for(i=0;i<n;i++)
+    {
+        if(a[i]>='0'&&a[i]<='9')
+        {
+            flag=0;
+        }
+        else
+        {
+            flag=1;
+            printf("no");
+            break;
+        }
+    }
+    if(flag==0)
+    {
+        printf("yes");
+    }
